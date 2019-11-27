1137c366b61fecabba64cf43da97fae50a866fb6
董国栋 <dgdxingche313@163.com>
2019-11-27 11:24:30 +0800

    初始创建

--- src/main/java/com/iot_rdss_gateway/IotRdssGatewayApplication.java
+++ src/main/java/com/iot_rdss_gateway/IotRdssGatewayApplication.java
@@ -1,13 +1,18 @@
 package com.iot_rdss_gateway;
 
+import com.iot_rdss_gateway.netty.server.NettyServer;
+import com.iot_rdss_gateway.util.SpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SpringUtil.class)
 public class IotRdssGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IotRdssGatewayApplication.class, args);
+        SpringUtil.getBean(NettyServer.class).run();
     }
 
 }
--- src/test/java/com/iot_rdss_gateway/IotRdssGatewayApplicationTests.java
+++ src/test/java/com/iot_rdss_gateway/IotRdssGatewayApplicationTests.java
@@ -1,6 +1,6 @@
 package com.iot_rdss_gateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
